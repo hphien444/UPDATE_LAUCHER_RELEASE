095011a70506255a3636e9d07d0b640f2d31656f
hphien444 <hphien444@users.noreply.github.com>
2026-05-19 22:24:44 +0700

    Fix release V8 snapshot payload

--- heads/fivereborn/production.php
+++ heads/fivereborn/production.php
@@ -2,7 +2,7 @@
 header('x-amz-meta-bootstrap-version: 1000016');
 header('x-amz-meta-bootstrap-size: 5082360');
 header('x-amz-meta-bootstrap-object: 4242fae1aa828e640196ec38d766b21bbef2f72191e860139018fa82cd08ebfb');
-header('x-amz-meta-branch-version: 1779202807');
-header('x-amz-meta-branch-manifest: f800f145d717f033b18da1bf43a0bf300bb6429c42327c4a8cfb91986f59b363');
+header('x-amz-meta-branch-version: 1779204003');
+header('x-amz-meta-branch-manifest: f6950807eff1bce774c3ee3be3fd30323b1560803ae84de73fdf786f00954641');
 echo "OK";
 ?>
