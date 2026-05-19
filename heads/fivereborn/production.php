08ddc275cad2ded5367fae62b9dc277b805a9ab8
hphien444 <hphien444@users.noreply.github.com>
2026-05-19 22:00:30 +0700

    Update release updater mirror

---
+++ heads/fivereborn/production.php
@@ -0,0 +1,8 @@
+<?php
+header('x-amz-meta-bootstrap-version: 1000016');
+header('x-amz-meta-bootstrap-size: 5082360');
+header('x-amz-meta-bootstrap-object: 4242fae1aa828e640196ec38d766b21bbef2f72191e860139018fa82cd08ebfb');
+header('x-amz-meta-branch-version: 1779202807');
+header('x-amz-meta-branch-manifest: f800f145d717f033b18da1bf43a0bf300bb6429c42327c4a8cfb91986f59b363');
+echo "OK";
+?>
