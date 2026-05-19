14cd8b2803fb17cbb361433c7bb64b51b858910c
hphien444 <hphien444@users.noreply.github.com>
2026-05-19 23:05:28 +0700

    Add release subprocess payloads

--- heads/fivereborn/production.php
+++ heads/fivereborn/production.php
@@ -2,7 +2,7 @@
 header('x-amz-meta-bootstrap-version: 1000016');
 header('x-amz-meta-bootstrap-size: 5082360');
 header('x-amz-meta-bootstrap-object: 4242fae1aa828e640196ec38d766b21bbef2f72191e860139018fa82cd08ebfb');
-header('x-amz-meta-branch-version: 1779204003');
-header('x-amz-meta-branch-manifest: f6950807eff1bce774c3ee3be3fd30323b1560803ae84de73fdf786f00954641');
+header('x-amz-meta-branch-version: 1779206546');
+header('x-amz-meta-branch-manifest: 0c3a8a67231b0d501cb45c527c81d9b91941bf02c0c190eba0d79ce2f89990f1');
 echo "OK";
 ?>
