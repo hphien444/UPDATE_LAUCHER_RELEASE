6f5e8e6512e8fffdb73cdcde2086a7bdb0bc0200
HIEN <hphien444@users.noreply.github.com>
2026-06-15 00:29:57 +0700

    Publish full launcher update with anticheat

--- heads/fivereborn/production.php
+++ heads/fivereborn/production.php
@@ -2,7 +2,7 @@
 header('x-amz-meta-bootstrap-version: 1000019');
 header('x-amz-meta-bootstrap-size: 5025176');
 header('x-amz-meta-bootstrap-object: dae763aea6ab09224c077ae2ed81ad6e822d9610a0458e2b7ef4fb08f641b708');
-header('x-amz-meta-branch-version: 1781448765');
-header('x-amz-meta-branch-manifest: 3108dbccf6d61588070ff33f7ae7229efa686ca874f4c7155aea3e8ecd8105df');
+header('x-amz-meta-branch-version: 1781457902');
+header('x-amz-meta-branch-manifest: 0fd07460cb7f2f8dfaf0d99695277dcbb8a6a4ac66202e1152e8e56265136e61');
 echo "OK";
 ?>
